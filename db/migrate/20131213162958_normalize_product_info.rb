1e484cb18c128a2d1f5cbc28e5ef320e5560a5c9
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2013-12-13 13:42:33 -0500

    Normalize Product migration

---
+++ db/migrate/20131213162958_normalize_product_info.rb
@@ -0,0 +1,21 @@
+class NormalizeProductInfo < ActiveRecord::Migration
+  def up
+    add_column :sales, :product_id, :integer
+    Sale.find_each do |sale|
+      product = Product.find_or_create_by(name: sale.product_name)
+      sale.product = product
+      sale.save
+    end
+    remove_column :sales, :product_name
+  end
+
+  def down
+    add_column :sales, :product_name, :string
+    Sale.find_each do |sale|
+      sale.product_name = sale.product.name
+      sale.save
+    end
+    remove_column :sales, :product_id
+    Product.delete_all
+  end
+end
